a8d649caa11a304f09e209bee4f1e3f4ede6d618
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-16 21:15:54 +0300

    Footer added to App Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import classes from './App.module.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Nav/Navbar';
 import Home from './components/layout/Pages/Home';
+import Footer from './components/layout/Footer/Footer';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <div className={classes.container}>
         <Home />
+        <Footer />
       </div>
     </Router>
   );
