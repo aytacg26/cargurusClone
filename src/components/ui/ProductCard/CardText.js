66ea4116580f7c537e0ef07f50c51a150455045c
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-16 16:15:02 +0300

    Product Card Component Created

---
+++ src/components/ui/ProductCard/CardText.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { textEllipsis } from '../../../utils/utilsFuncs';
+import classes from './CardText.module.css';
+
+const CardText = ({ header, author, text }) => {
+  const revisedText = textEllipsis(text, 35);
+
+  return (
+    <div className={classes.TextSection}>
+      <h2>{header}</h2>
+      <span>{author}</span>
+      <p>{revisedText}</p>
+    </div>
+  );
+};
+
+export default CardText;
--- src/components/ui/ProductCard/ProductCard.js
+++ src/components/ui/ProductCard/ProductCard.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import classes from './ProductCard.module.css';
+import imageLoader from '../../../assets/images/BodyStyles/ajax-loader.gif';
+import { loadImage } from '../../../utils/utilsFuncs';
+import CardText from './CardText';
 
-const ProductCard = () => {
+const ProductCard = ({ id, image, header, author, text }) => {
+  const imageRef = useRef();
+
+  useEffect(() => {
+    loadImage(image, imageRef.current);
+  }, [image]);
+
   return (
-    <div className={classes.ProductCard}>
-      <div>Image Section</div>
-      <div>Text Section, This must be link</div>
-    </div>
+    <Link to={`/articles/${id}`} className={classes.ProductCard}>
+      <div className={classes.ImageSection}>
+        <img
+          src={imageLoader}
+          alt={header}
+          title={`${header} - ${author}`}
+          ref={imageRef}
+        />
+      </div>
+      <CardText header={header} author={author} text={text} />
+    </Link>
   );
 };
 
