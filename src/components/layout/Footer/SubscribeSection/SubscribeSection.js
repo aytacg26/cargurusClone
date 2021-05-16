dad70ee4a5a61be6665650a58f390d432b13c7f3
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-16 21:10:34 +0300

    Footer Subscribe Section Component Created

---
+++ src/components/layout/Footer/SubscribeSection/SubscribeSection.js
@@ -0,0 +1,50 @@
+import React, { Fragment, useState } from 'react';
+import { emailValidation } from '../../../../utils/utilsFuncs';
+import Button from '../../../ui/Button/Button';
+import Input from '../../../ui/Input/Input';
+import classes from './SubscribeSection.module.css';
+
+const SubscribeSection = () => {
+  const [subscribe, setSubscribe] = useState('');
+  const [notValid, setNotValid] = useState(false);
+
+  const handleSubscribe = (e) => {
+    setNotValid(false);
+    setSubscribe(e.target.value);
+  };
+
+  const handleSubscribeSubmit = (e) => {
+    e.preventDefault();
+    const isValidEmail = emailValidation(subscribe);
+
+    if (isValidEmail) {
+      console.log(subscribe);
+    } else {
+      setNotValid(true);
+    }
+  };
+
+  return (
+    <div className={classes.SubscribeContainer}>
+      <span className={classes.SubscribeHeading}>
+        Sign up for Price Drops, New Listings, & More
+      </span>
+      <form className={classes.Subscribe} onSubmit={handleSubscribeSubmit}>
+        <Input
+          type='text'
+          name='subscribe'
+          value={subscribe}
+          placeholder='Email Address'
+          onChange={handleSubscribe}
+          notValid={notValid}
+          errorMessage='Please enter a valid email address...'
+        />
+        <Button btnType='success' style={{ height: 45, width: 100 }}>
+          Subscribe
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default SubscribeSection;
