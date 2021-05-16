37c9eee078259f21a692b08fd23374939e332840
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-16 21:09:50 +0300

    Imput component improved

--- src/components/ui/Input/Input.js
+++ src/components/ui/Input/Input.js
@@ -3,13 +3,28 @@ import classes from './Input.module.css';
 
 const Input = (props) => {
   const label = props.label;
+  const warningMessage = props.errorMessage;
+  const notValid = props.notValid;
   const inputProps = { ...props };
   delete inputProps.label;
+  delete inputProps.notValid;
+  delete inputProps.errorMessage;
+
+  const inputClass = `${classes.Input} ${notValid ? classes.NotValid : ''}`;
+  const warningClass = `${classes.WarningContainer} ${
+    notValid ? classes.Show : ''
+  }`;
 
   return (
     <label>
       {label}
-      <input {...inputProps} className={classes.Input} />
+      <input {...inputProps} className={inputClass} autoComplete='off' />
+      {warningMessage && (
+        <div className={warningClass}>
+          <span className={classes.Triangle}></span>
+          <div className={classes.Warning}>{warningMessage}</div>
+        </div>
+      )}
     </label>
   );
 };
