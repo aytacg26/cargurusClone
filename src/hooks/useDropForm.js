47fc3966e11bae6fb32f93d162399a67a98bfa46
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-19 23:22:07 +0300

    additional selection added to useDropForm Hook.

--- src/hooks/useDropForm.js
+++ src/hooks/useDropForm.js
@@ -4,6 +4,7 @@ export const useDropForm = (dropNamesArray) => {
   const [firstDrop, setFirstDrop] = useState(false);
   const [secondDrop, setSecondDrop] = useState(false);
   const [thirdDrop, setthirdDrop] = useState(false);
+  const [fourthDrop, setfourthDrop] = useState(false);
 
   const handleDropClick = useCallback(
     (name) => {
@@ -12,16 +13,26 @@ export const useDropForm = (dropNamesArray) => {
           setFirstDrop((prevState) => !prevState);
           setSecondDrop(false);
           setthirdDrop(false);
+          setfourthDrop(false);
           break;
         case dropNamesArray[1]:
           setFirstDrop(false);
           setSecondDrop((prevState) => !prevState);
           setthirdDrop(false);
+          setfourthDrop(false);
           break;
         case dropNamesArray[2]:
           setFirstDrop(false);
           setSecondDrop(false);
           setthirdDrop((prevState) => !prevState);
+          setfourthDrop(false);
+          break;
+
+        case dropNamesArray[3]:
+          setFirstDrop(false);
+          setSecondDrop(false);
+          setthirdDrop(false);
+          setfourthDrop((prevState) => !prevState);
           break;
 
         default:
@@ -43,5 +54,5 @@ export const useDropForm = (dropNamesArray) => {
     };
   }, [handleDropClick]);
 
-  return { firstDrop, secondDrop, thirdDrop, handleDropClick };
+  return { firstDrop, secondDrop, thirdDrop, fourthDrop, handleDropClick };
 };
