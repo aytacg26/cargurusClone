5e3171d5602f72ee3acfeaeabda85d691235e2d5
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-19 23:15:26 +0300

    Used and News Cars Routes Added to App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import classes from './App.module.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Nav/Navbar';
-import Home from './components/layout/Pages/Home';
+import Home from './components/layout/Pages/Home/Home';
 import Footer from './components/layout/Footer/Footer';
+import UsedCars from './components/layout/Pages/UsedCars/UsedCars';
+import NewCars from './components/layout/Pages/NewCars/NewCars';
+import ByBody from './components/layout/SearchWindow/ByBody/ByBody';
+import DropDown from './components/ui/DropDown/DropDown';
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <div className={classes.container}>
-        <Home />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/used-cars' exact component={UsedCars} />
+          <Route path='/new-cars' exact component={NewCars} />
+        </Switch>
         <Footer />
       </div>
     </Router>
