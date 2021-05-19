b9e2727dbbd464864b82b0cf55d9263f60233183
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-19 23:21:23 +0300

    New props and styling added to DropDown Comp.

--- src/components/ui/DropDown/DropDown.js
+++ src/components/ui/DropDown/DropDown.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import DropMenu from '../../layout/Nav/DropMenu';
 import classes from './DropDown.module.css';
 import { FaChevronDown } from 'react-icons/fa';
+import { numberList } from '../../../utils/utilsFuncs';
 
 const DropDown = ({
   selectOptions,
@@ -12,25 +13,75 @@ const DropDown = ({
   onClick,
   drop,
   name,
+  type,
+  max,
+  min,
+  step,
+  style,
+  placeHolder,
+  reverse,
 }) => {
   const [options, setOptions] = useState([]);
   const [selected, setSelected] = useState({});
+  const [title, setTitle] = useState(titleKey);
+  const [value, setValue] = useState(valueKey);
   const iconClass = drop ? `${classes.icon} ${classes.dropped}` : classes.icon;
 
   useEffect(() => {
-    const revisedOps = selectOptions.map((option) =>
-      option.id === defaultOptionId
-        ? { ...option, selected: true }
-        : { ...option, selected: false }
-    );
+    if (selectOptions && !type) {
+      const revisedOps = selectOptions.map((option) =>
+        option.id === defaultOptionId
+          ? { ...option, selected: true }
+          : { ...option, selected: false }
+      );
 
-    const defaultOption = revisedOps.find(
-      (option) => option.id === defaultOptionId
-    );
+      const defaultOption = revisedOps.find(
+        (option) => option.id === defaultOptionId
+      );
 
-    setSelected(defaultOption);
-    setOptions(revisedOps);
-  }, [defaultOptionId, selectOptions]);
+      setSelected(defaultOption);
+      setOptions(revisedOps);
+    }
+
+    if (!selectOptions && type && typeof type === 'string') {
+      let numOptions = [];
+      let revisedOps = [];
+      if (type.toLowerCase() === 'number') {
+        const incStep = step || 1;
+        numOptions = numberList(min, max, incStep, true);
+        numOptions.unshift({
+          id: 'nums-all-001-009',
+          name: placeHolder || 'All Numbers',
+          value: 'all',
+        });
+      }
+
+      if (type.toLowerCase() === 'year') {
+        const year = new Date().getFullYear();
+        const initYear = min || 1930;
+        const incStep = step || 1;
+        numOptions = reverse
+          ? numberList(initYear, year, incStep, true).reverse()
+          : numberList(initYear, year, incStep, true);
+        numOptions.unshift({
+          id: 'year-0001-0-all',
+          name: 'All Years',
+          value: 'all',
+        });
+      }
+
+      revisedOps = numOptions.map((option, index) =>
+        index === 0
+          ? { ...option, selected: true }
+          : { ...option, selected: false }
+      );
+
+      setSelected(revisedOps[0]);
+      setOptions(revisedOps);
+      setTitle('name');
+      setValue('value');
+    }
+  }, [defaultOptionId, selectOptions, type, min, max, placeHolder, step]);
 
   const handleOptionSelect = (id) => {
     const selectedOption = options.find((option) => option.id === id);
@@ -42,11 +93,15 @@ const DropDown = ({
 
     setOptions(updatedOptions);
     setSelected(selectedOption);
-    onSelect && onSelect(selectedOption[valueKey]);
+    onSelect && onSelect(selectedOption[value]);
   };
 
+  if (options.length === 0) {
+    return null;
+  }
+
   return (
-    <div className={classes.DropDown}>
+    <div className={classes.DropDown} style={style}>
       <button
         className={classes.DropBtn}
         onClick={() => {
@@ -55,7 +110,7 @@ const DropDown = ({
         type='button'
         name={name}
       >
-        <span>{selected[titleKey]}</span>
+        <span>{selected[title]}</span>
         <span>
           <FaChevronDown
             size='16px'
@@ -65,7 +120,7 @@ const DropDown = ({
         </span>
       </button>
       {drop && (
-        <DropMenu top='39px' left='5px'>
+        <DropMenu top='39px'>
           {options.map((option) => (
             <span
               className={`${classes.Option} ${
@@ -74,7 +129,7 @@ const DropDown = ({
               key={option.id}
               onClick={() => handleOptionSelect(option.id)}
             >
-              {option[titleKey]}
+              {option[title]}
             </span>
           ))}
         </DropMenu>
