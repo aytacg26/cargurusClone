488d52203828151f5b1c80360b76708374f1d619
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-19 23:19:25 +0300

    New props added to CarBodyCard comp. for styling

--- src/components/layout/SearchWindow/ByBody/CarBodyCard.js
+++ src/components/layout/SearchWindow/ByBody/CarBodyCard.js
@@ -1,10 +1,19 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import classes from './CarBodyCard.module.css';
 import { FaCheckCircle } from 'react-icons/fa';
 import loading from '../../../../assets/images/BodyStyles/ajax-loader.gif';
 import { loadImage } from '../../../../utils/utilsFuncs';
 
-const CarBodyCard = ({ title, image, onSelect, selected }) => {
+const CarBodyCard = ({
+  title,
+  image,
+  onSelect,
+  selected,
+  to,
+  style,
+  noBorder,
+}) => {
   const hoverableClass = `${classes.Hover} ${selected ? classes.selected : ''}`;
   const [showTitle, setShowTitle] = useState(false);
   const imageRef = useRef();
@@ -13,15 +22,33 @@ const CarBodyCard = ({ title, image, onSelect, selected }) => {
     loadImage(image, imageRef.current, setShowTitle);
   }, [image]);
 
-  return (
-    <div className={classes.CarBodyCard}>
+  const content = (
+    <Fragment>
       <img src={loading} alt={title} title={title} ref={imageRef} />
-      {showTitle && <span>{title}</span>}
-      <span className={hoverableClass} onClick={onSelect}>
-        {selected && (
-          <FaCheckCircle color='#0277bd' style={{ margin: '4px' }} />
-        )}
-      </span>
+      {showTitle && (
+        <Fragment>
+          <span>{title}</span>
+          <span
+            className={hoverableClass}
+            onClick={onSelect}
+            style={{ border: noBorder ? '0 none' : '' }}
+          >
+            {selected && (
+              <FaCheckCircle color='#0277bd' style={{ margin: '4px' }} />
+            )}
+          </span>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+
+  return to ? (
+    <Link to={to} className={classes.CarBodyCard} style={style}>
+      {content}
+    </Link>
+  ) : (
+    <div className={classes.CarBodyCard} style={style}>
+      {content}
     </div>
   );
 };
