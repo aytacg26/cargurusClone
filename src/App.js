2616af629609de5f1db94838cce7881cf4c9b4f7
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-20 19:12:54 +0300

    Provider and Redux Store added to App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import classes from './App.module.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import appStore from './store/store';
 import Navbar from './components/layout/Nav/Navbar';
 import Home from './components/layout/Pages/Home/Home';
 import Footer from './components/layout/Footer/Footer';
 import UsedCars from './components/layout/Pages/UsedCars/UsedCars';
 import NewCars from './components/layout/Pages/NewCars/NewCars';
-import ByBody from './components/layout/SearchWindow/ByBody/ByBody';
-import DropDown from './components/ui/DropDown/DropDown';
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <div className={classes.container}>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/used-cars' exact component={UsedCars} />
-          <Route path='/new-cars' exact component={NewCars} />
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+    <Provider store={appStore}>
+      <Router>
+        <Navbar />
+        <div className={classes.container}>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/used-cars' exact component={UsedCars} />
+            <Route path='/new-cars' exact component={NewCars} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
   );
 };
 
