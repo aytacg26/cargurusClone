361f9570cc51426093bc16e87b08433f189f7bbd
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-20 21:29:51 +0300

    image loader added to Brand Card component

--- src/components/ui/BrandCard/BrandCard.js
+++ src/components/ui/BrandCard/BrandCard.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import { loadImage } from '../../../utils/utilsFuncs';
 import classes from './BrandCard.module.css';
+import imageLoader from '../../../assets/images/BodyStyles/ajax-loader.gif';
 
 const BrandCard = ({ brand, id, logo, company }) => {
+  const imageRef = useRef();
+
+  useEffect(() => {
+    loadImage(logo, imageRef.current);
+  }, [logo]);
+
   return (
     <Link to={`/brandcars/${brand}/${id}`} className={classes.BrandCard}>
       <div className={classes.ImageSection}>
-        <img src={logo} alt={brand} title={brand} width='140' height='140' />
+        <img
+          src={imageLoader}
+          alt={brand}
+          title={brand}
+          width='140'
+          height='140'
+          ref={imageRef}
+        />
       </div>
       <div className={classes.textSection}>
         <h3>{brand}</h3>
