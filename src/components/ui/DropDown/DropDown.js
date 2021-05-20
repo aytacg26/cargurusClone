cffa5edaf80d312a63d1a5de93ee80efead7310d
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-20 19:11:57 +0300

    DropDown comp. updated and currently works better

--- src/components/ui/DropDown/DropDown.js
+++ src/components/ui/DropDown/DropDown.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import DropMenu from '../../layout/Nav/DropMenu';
 import classes from './DropDown.module.css';
-import { FaChevronDown } from 'react-icons/fa';
+import { FaChevronDown, FaTimes } from 'react-icons/fa';
 import { numberList } from '../../../utils/utilsFuncs';
 
 const DropDown = ({
@@ -20,15 +20,56 @@ const DropDown = ({
   style,
   placeHolder,
   reverse,
+  onClear,
+  dropValue,
 }) => {
   const [options, setOptions] = useState([]);
   const [selected, setSelected] = useState({});
+  const [dropVal, setDropVal] = useState(null);
+  const [initValState, setInitValState] = useState(dropValue);
   const [title, setTitle] = useState(titleKey);
   const [value, setValue] = useState(valueKey);
-  const iconClass = drop ? `${classes.icon} ${classes.dropped}` : classes.icon;
+  const [clearFilter, setClearFilter] = useState(false);
+  const [dropDown, setDropDown] = useState(drop);
+  // let iconClass = drop ? `${classes.icon} ${classes.dropped}` : classes.icon;
+  let iconClass = dropDown
+    ? `${classes.icon} ${classes.dropped}`
+    : classes.icon;
+  const dropDownRef = useRef();
+
+  console.log('dropdown running...', name);
+  console.log(
+    '----------------------------------------------------------------------------------------'
+  );
 
   useEffect(() => {
-    if (selectOptions && !type) {
+    if (dropDown) {
+      const handleClick = (event) => {
+        if (event.target.name !== name) {
+          setDropDown(false);
+        } else {
+          setDropDown((prevState) => !prevState);
+        }
+      };
+
+      const handleKeyUp = (e) => {
+        if (e.key === 'escape' || e.which === 27) {
+          setDropDown(false);
+        }
+      };
+
+      document.addEventListener('click', handleClick);
+      document.addEventListener('keyup', handleKeyUp);
+
+      return () => {
+        document.removeEventListener('click', handleClick);
+        document.removeEventListener('keyup', handleKeyUp);
+      };
+    }
+  }, [name, dropDown]);
+
+  useEffect(() => {
+    if (selectOptions && !type && !clearFilter) {
       const revisedOps = selectOptions.map((option) =>
         option.id === defaultOptionId
           ? { ...option, selected: true }
@@ -43,31 +84,39 @@ const DropDown = ({
       setOptions(revisedOps);
     }
 
-    if (!selectOptions && type && typeof type === 'string') {
+    if (!selectOptions && type && typeof type === 'string' && !clearFilter) {
       let numOptions = [];
       let revisedOps = [];
+
       if (type.toLowerCase() === 'number') {
         const incStep = step || 1;
         numOptions = numberList(min, max, incStep, true);
-        numOptions.unshift({
-          id: 'nums-all-001-009',
-          name: placeHolder || 'All Numbers',
-          value: 'all',
-        });
+
+        if (!clearFilter) {
+          numOptions.unshift({
+            id: 'nums-all-001-009',
+            name: placeHolder || 'All Numbers',
+            value: 'all',
+          });
+        }
       }
 
       if (type.toLowerCase() === 'year') {
-        const year = new Date().getFullYear();
-        const initYear = min || 1930;
+        const year =
+          (typeof max === 'number' && max) || new Date().getFullYear();
+        const initYear = (typeof min === 'number' && min) || 1930;
         const incStep = step || 1;
         numOptions = reverse
           ? numberList(initYear, year, incStep, true).reverse()
           : numberList(initYear, year, incStep, true);
-        numOptions.unshift({
-          id: 'year-0001-0-all',
-          name: 'All Years',
-          value: 'all',
-        });
+
+        if (!clearFilter) {
+          numOptions.unshift({
+            id: 'year-0001-0-all',
+            name: 'All Years',
+            value: 'all',
+          });
+        }
       }
 
       revisedOps = numOptions.map((option, index) =>
@@ -81,7 +130,27 @@ const DropDown = ({
       setTitle('name');
       setValue('value');
     }
-  }, [defaultOptionId, selectOptions, type, min, max, placeHolder, step]);
+    //eslint-disable-next-line
+  }, [
+    defaultOptionId,
+    selectOptions,
+    type,
+    placeHolder,
+    step,
+    reverse,
+    min,
+    max,
+    clearFilter,
+  ]);
+
+  useEffect(() => {
+    if (dropValue && dropValue !== initValState) {
+      setDropVal(dropValue);
+    } else {
+      setClearFilter(false);
+      setInitValState(dropValue);
+    }
+  }, [dropValue, dropVal, initValState]);
 
   const handleOptionSelect = (id) => {
     const selectedOption = options.find((option) => option.id === id);
@@ -93,7 +162,13 @@ const DropDown = ({
 
     setOptions(updatedOptions);
     setSelected(selectedOption);
+    setClearFilter(true);
     onSelect && onSelect(selectedOption[value]);
+  };
+
+  const handleClear = () => {
+    onClear && onClear();
+    setClearFilter(false);
   };
 
   if (options.length === 0) {
@@ -101,16 +176,17 @@ const DropDown = ({
   }
 
   return (
-    <div className={classes.DropDown} style={style}>
+    <div className={classes.DropDown} style={style} ref={dropDownRef}>
       <button
         className={classes.DropBtn}
         onClick={() => {
           onClick && onClick(name);
+          setDropDown(true);
         }}
         type='button'
         name={name}
       >
-        <span>{selected[title]}</span>
+        {selected[title]}
         <span>
           <FaChevronDown
             size='16px'
@@ -119,7 +195,24 @@ const DropDown = ({
           />
         </span>
       </button>
-      {drop && (
+      {clearFilter && (
+        <span>
+          <FaTimes
+            style={{
+              margin: '3px 25px 0 10px',
+              background: 'transparent',
+              width: 18,
+              height: 18,
+              cursor: 'pointer',
+              borderRadius: '50%',
+            }}
+            className={classes.icon}
+            color='red'
+            onClick={handleClear}
+          />
+        </span>
+      )}
+      {dropDown && (
         <DropMenu top='39px'>
           {options.map((option) => (
             <span
@@ -138,4 +231,4 @@ const DropDown = ({
   );
 };
 
-export default DropDown;
+export default React.memo(DropDown);
