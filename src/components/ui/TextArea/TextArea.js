4bf23b347c6494097806597e3bc2d73533ef791e
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-22 22:15:12 +0300

    Auto Resizing TextArea component created

---
+++ src/components/ui/TextArea/TextArea.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect, useRef } from 'react';
+import classes from './TextArea.module.css';
+
+const TextArea = ({
+  onChange,
+  placeholder,
+  name,
+  title,
+  value,
+  maxLength,
+  counterText,
+  showCounter,
+}) => {
+  const [rowSize, setRowSize] = useState(4);
+  const [currentScrollHeight, setCurrentScrollHeight] = useState(null);
+  const [counter, setCounter] = useState(0);
+  const [counterInit, setCounterInit] = useState(0);
+  const textAreaRef = useRef();
+
+  useEffect(() => {
+    setCurrentScrollHeight(textAreaRef.current.scrollHeight);
+  }, []);
+
+  useEffect(() => {
+    const countValue = maxLength || 500;
+    setCounter(countValue);
+    setCounterInit(countValue);
+  }, [maxLength]);
+
+  const handleChange = (e) => {
+    onChange && onChange(e);
+
+    setCounter((prevState) => {
+      if (counterInit - prevState > e.target.value.length) {
+        return prevState + 1;
+      } else {
+        return prevState - 1;
+      }
+    });
+
+    if (e.target.scrollHeight > currentScrollHeight) {
+      setRowSize((prevState) => prevState + 1);
+      setCurrentScrollHeight(e.target.scrollHeight);
+    }
+  };
+
+  return (
+    <label className={classes.textareaLabel}>
+      <span>Label</span>
+      <textarea
+        rows={rowSize}
+        cols='50'
+        placeholder={placeholder}
+        name={name}
+        title={title}
+        maxLength={counterInit}
+        value={value}
+        className={classes.TextArea}
+        onChange={handleChange}
+        ref={textAreaRef}
+        style={{ marginBottom: !showCounter ? '15px' : '' }}
+      ></textarea>
+      {showCounter && (
+        <div className={classes.remainingAmount}>
+          <span className={classes.CounterText}>
+            {counterText || 'Remaining : '}
+          </span>
+          <span className={classes.MaxLength}>{counter}</span>
+        </div>
+      )}
+    </label>
+  );
+};
+
+export default TextArea;
