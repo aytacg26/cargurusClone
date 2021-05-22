40ecf21386834e48fec177d083328c2628225699
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-22 18:37:23 +0300

    Rating Star Presentation Component Created

---
+++ src/components/ui/RatingStars/RatingStarPresentation/RatingStarPresentation.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Star from '../Star/Star';
+import classes from './RatingStarPresentation.module.css';
+import { starObjArray } from '../../../../utils/utilsFuncs';
+
+const RatingStarPresentation = ({ rate }) => {
+  const content = starObjArray(rate);
+
+  return (
+    <div
+      className={classes.Stars}
+      title={`This dealer has an average rating of ${rate}`}
+    >
+      {content.map((star) => (
+        <Star starType={star.starType} key={star.id} />
+      ))}
+      <span className={classes.StarRate}>{`(${rate}/5)`}</span>
+    </div>
+  );
+};
+
+export default RatingStarPresentation;
