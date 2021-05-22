b822412f630c255418b1c17d950b194b625833ae
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-22 18:31:30 +0300

    Initial Structure of Product page created

---
+++ src/components/layout/Pages/ProductPage/ProductPage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PageContainer from '../PageContainer/PageContainer';
+import classes from './ProductPage.module.css';
+import ShareData from './ShareData/ShareData';
+import DealerInteraction from './DealerInteraction/DealerInteraction';
+import ProductSection from './ProductSection/ProductSection';
+import HeaderSection from './HeaderSection/HeaderSection';
+
+const ProductPage = (props) => {
+  return (
+    <PageContainer>
+      <div className={classes.ProductDetailsContainer}>
+        <HeaderSection
+          productHeader='BMW M3 Convertible Silver'
+          dealer='Luxury Autos Ltd.'
+          location='Nicosia'
+        />
+        <div className={classes.DetailsSections}>
+          <ShareData />
+          <ProductSection />
+          <DealerInteraction
+            rate={3.4}
+            numberOfReviews={25}
+            dealStatus='good'
+            priceDifference={3500}
+          />
+        </div>
+      </div>
+    </PageContainer>
+  );
+};
+
+export default ProductPage;
