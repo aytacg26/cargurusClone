8c7459cea9e6ce53c1f277a1911847ae87e8ec7a
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-23 16:59:08 +0300

    Message Form Header style and made optional

--- src/components/layout/Pages/ProductPage/MessageForm/MessageForm.js
+++ src/components/layout/Pages/ProductPage/MessageForm/MessageForm.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
+import {
+  emailValidation,
+  textValidation,
+} from '../../../../../utils/utilsFuncs';
 import Button from '../../../../ui/Button/Button';
+import Envelope from '../../../../ui/Envelope/Envelope';
 import Input from '../../../../ui/Input/Input';
 import TextArea from '../../../../ui/TextArea/TextArea';
+import FormSuccess from './FormSuccess/FormSuccess';
 import classes from './MessageForm.module.css';
 
-const MessageForm = () => {
+const MessageForm = ({ formHeader, dealerPhone }) => {
   const [formData, setFormData] = useState({
     name: '',
     surname: '',
@@ -13,50 +19,162 @@ const MessageForm = () => {
     message: '',
   });
 
+  const { name, surname, email, phone, message } = formData;
+
+  const [nameIsValid, setNameIsValid] = useState(false);
+  const [surnameIsValid, setSurnameIsValid] = useState(false);
+  const [emailIsValid, setEmailIsValid] = useState(false);
+  const [messageIsValid, setMessageIsValid] = useState(false);
+  const [disabledBtn, setDisabledBtn] = useState(false);
+  const [sending, setSending] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
   const handleFormChange = (e) => {
+    e.target.name === 'name' && setNameIsValid(false);
+    e.target.name === 'surname' && setSurnameIsValid(false);
+    e.target.name === 'email' && setEmailIsValid(false);
+    e.target.name === 'message' && setMessageIsValid(false);
+
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
   };
 
-  const handleMessageSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
+  const handleInputBlur = (e) => {
+    if (e.target.value.trim().length === 0) {
+      e.target.name === 'name' && setNameIsValid(true);
+      e.target.name === 'surname' && setSurnameIsValid(true);
+      e.target.name === 'email' && setEmailIsValid(true);
+      e.target.name === 'message' && setMessageIsValid(true);
+    }
+
+    if (e.target.name === 'email' && !emailValidation(e.target.value)) {
+      setEmailIsValid(true);
+    }
   };
 
-  return (
-    <div className={classes.FormContainer}>
-      <span>Form Header...</span>
+  const handleMessageSubmit = async (e) => {
+    e.preventDefault();
+
+    const showForm = (time) => {
+      const timer = setTimeout(() => {
+        setSuccess(false);
+        setError(false);
+        setSending(false);
+        setDisabledBtn(false);
+        clearTimeout(timer);
+      }, time);
+    };
+
+    const isValidName = textValidation(name, 35);
+    const isValidSurname = textValidation(surname, 35);
+    const isValidEmail = emailValidation(email);
+    const isValidMessage = textValidation(message, 650);
+
+    if (isValidName && isValidSurname && isValidEmail && isValidMessage) {
+      setDisabledBtn(true);
+      setSending(true);
+      const messageForm = {
+        dealerId: '0000000000000', //get this from the product page
+        productId: '000001111111', //get this from the product page
+        messageSendAt: new Date(),
+        name,
+        surname,
+        email,
+        phone,
+        message,
+      };
+
+      console.log(messageForm);
+      //message send to the message box of the dealer id with async func. Fetch API or axios will be used.
+      //If the message send successfully, form should cleaned up, and a success message window should be shown. else an error window will be
+      //shown and there won't be any cleanup process on form
+      //const res = await fetch("/api/messages/dealerId", {method:"POST", headers:{}, body:messageForm})
+
+      let res = true; //if this is set to false, it will show error message, this is just to mimic the response from backend
+      // after creating backend, this part will be revised accordingly.
+
+      //To Mimic lateness of async fetch process
+
+      setTimeout(() => {
+        if (res) {
+          setSuccess(true);
+          setFormData({
+            name: '',
+            surname: '',
+            email: '',
+            phone: '',
+            message: '',
+          });
+
+          //After showing success message, show empty form within 4 seconds
+          showForm(3500);
+        } else {
+          setError(true);
+          //after error message, show form with data within 5 seconds
+          showForm(4500);
+        }
+      }, 2500);
+    } else {
+      setNameIsValid(!isValidName);
+      setSurnameIsValid(!isValidSurname);
+      setEmailIsValid(!isValidEmail);
+      setMessageIsValid(!isValidMessage);
+    }
+  };
+
+  const formContent = (
+    <Fragment>
+      {formHeader && (
+        <div className={classes.FormHeader}>
+          <span className={classes.header}>{formHeader}</span>
+          <span>{dealerPhone}</span>
+        </div>
+      )}
       <form autoComplete='off' onSubmit={handleMessageSubmit}>
         <div className={classes.FormGroup}>
           <Input
             label='Name'
-            notValid={false}
+            notValid={nameIsValid}
             type='text'
             name='name'
             value={formData.name}
             onChange={handleFormChange}
+            errorMessage='Please enter your name.'
+            useTextError
+            onBlur={handleInputBlur}
           />
         </div>
         <div className={classes.FormGroup}>
           <Input
             label='Surname'
-            notValid={false}
+            notValid={surnameIsValid}
             type='text'
             name='surname'
             value={formData.surname}
             onChange={handleFormChange}
+            errorMessage='Please enter your surname'
+            useTextError
+            onBlur={handleInputBlur}
           />
         </div>
         <div className={classes.FormGroup}>
           <Input
             label='Email'
-            notValid={false}
+            notValid={emailIsValid}
             type='text'
             name='email'
             value={formData.email}
             onChange={handleFormChange}
+            onBlur={handleInputBlur}
+            errorMessage={
+              email === ''
+                ? 'Please enter your email address'
+                : 'Please enter a valid email address.'
+            }
+            useTextError
           />
         </div>
         <div className={classes.FormGroup}>
@@ -72,14 +190,34 @@ const MessageForm = () => {
         <div className={classes.FormGroup}>
           <TextArea
             onChange={handleFormChange}
+            onBlur={handleInputBlur}
             name='message'
             value={formData.message}
             showCounter
             maxLength={650}
+            label='Message'
+            warningMessage='Please enter your message.'
+            notValid={messageIsValid}
+            useTextError
           />
         </div>
-        <Button btnType='success'>Send</Button>
+        <Button btnType='success' disabled={disabledBtn}>
+          Send
+        </Button>
       </form>
+    </Fragment>
+  );
+
+  return (
+    <div className={classes.FormContainer}>
+      {!success && !error && formContent}
+      {(success || error) && <FormSuccess isError={error} />}
+
+      {sending && (
+        <div className={classes.formModal}>
+          <Envelope sending={sending} />
+        </div>
+      )}
     </div>
   );
 };
