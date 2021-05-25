e678f0807d2ad71dd682369ab58416104724fd46
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-26 01:08:13 +0300

    New parts added to Product Section component

--- src/components/layout/Pages/ProductPage/ProductSection/ProductSection.js
+++ src/components/layout/Pages/ProductPage/ProductSection/ProductSection.js
@@ -1,14 +1,104 @@
-import React from 'react';
+import React, { useState, useMemo, Fragment, lazy, Suspense } from 'react';
+import Loader from '../../../../ui/Loader/Loader';
+// import Modal from '../../../../ui/Modal/Modal';
+import Financing from '../../../Financing/Financing';
+import DetailIconsList from './DetailIconsList/DetailIconsList';
 import classes from './ProductSection.module.css';
 
+const Modal = lazy(() => import('../../../../ui/Modal/Modal'));
+
+//car details will come from database and we will get this data directly to this component from Redux
+//for test purpose, static data used.
 const ProductSection = () => {
+  const [hideFinancingModal, setHideFinancingModal] = useState(true);
+
+  const handleFinancingModal = () => {
+    setHideFinancingModal((prevState) => !prevState);
+  };
+
+  const carDetails = useMemo(
+    () => ({
+      milage: '30,775 miles',
+      owner: '1 owner',
+      gearbox: '6 speed Automatic',
+      driveTrain: 'Front-Wheel Drive',
+      fuelType: 'Gasoline',
+      engine: 'I4',
+      fuelEconomy: '30 MPG (combined)',
+      interiorColor: 'Black',
+    }),
+    []
+  );
+
   return (
-    <div className={classes.ProductSection}>
-      <div className={classes.ImageBox}>ImageBox</div>
-      <div>Product Summary and Details</div>
-      <span>Gallery Contact Details</span>
-      <div id='reviews'>Reviews</div>
-    </div>
+    <Fragment>
+      <div className={classes.ProductSection}>
+        <div className={classes.ImageBox}>ImageBox</div>
+        <DetailIconsList carDetails={carDetails} header='Vehicle Details' />
+        <Financing
+          header='Finance in Advance'
+          onClick={handleFinancingModal}
+          btnTitle='See my options'
+          btnType='success'
+        />
+        <div className={classes.DetailsText}>Details Text Section</div>
+        <div id='reviews' className={classes.Reviews}>
+          Reviews
+        </div>
+      </div>
+      <Suspense fallback={<Loader />}>
+        <Modal onClick={handleFinancingModal} hide={hideFinancingModal}>
+          <div className={classes.FinancingContainer}>
+            <h3>Overview</h3>
+
+            <Financing
+              header='Shop with real rates in hand'
+              btnType='primary'
+              btnTitle='Get Started'
+              style={{ width: '100%' }}
+              onClick={() => console.log('Open next page on modal...')}
+            />
+            <div className={classes.FinancingPolicy}>
+              <p>
+                Take this Modal to another folder as a seperate component, this
+                will have more other parts which we will need to show up on
+                button click...
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A, eum
+                temporibus. Minima tempore, veniam facilis aperiam labore
+                deserunt nulla quibusdam doloribus amet quia deleniti ratione,
+                quos quas? Possimus, iusto molestias.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A, eum
+                temporibus. Minima tempore, veniam facilis aperiam labore
+                deserunt nulla quibusdam doloribus amet quia deleniti ratione,
+                quos quas? Possimus, iusto molestias.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A, eum
+                temporibus. Minima tempore, veniam facilis aperiam labore
+                deserunt nulla quibusdam doloribus amet quia deleniti ratione,
+                quos quas? Possimus, iusto molestias.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A, eum
+                temporibus. Minima tempore, veniam facilis aperiam labore
+                deserunt nulla quibusdam doloribus amet quia deleniti ratione,
+                quos quas? Possimus, iusto molestias.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A, eum
+                temporibus. Minima tempore, veniam facilis aperiam labore
+                deserunt nulla quibusdam doloribus amet quia deleniti ratione,
+                quos quas? Possimus, iusto molestias.
+              </p>
+            </div>
+          </div>
+        </Modal>
+      </Suspense>
+    </Fragment>
   );
 };
 
