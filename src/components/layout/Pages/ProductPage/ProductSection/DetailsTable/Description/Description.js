f42e16173702651ed083c24dd45227dead34588e
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-30 18:29:37 +0300

    Description component created for Dealer's Desc.

---
+++ src/components/layout/Pages/ProductPage/ProductSection/DetailsTable/Description/Description.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import classes from './Description.module.scss';
+
+const Description = ({ header, text }) => {
+  return (
+    <div className={classes.DescriptionContainer}>
+      <h2>Dealer's Description</h2>
+      <div className={classes.Description}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, rerum
+          quidem molestias fugit eligendi nihil unde recusandae ipsa laudantium
+          minima aspernatur dolores laborum! Repudiandae pariatur vitae adipisci
+          maiores doloribus dolores. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Dolor, rerum quidem molestias fugit eligendi nihil
+          unde recusandae ipsa laudantium minima aspernatur dolores laborum!
+          Repudiandae pariatur vitae adipisci maiores doloribus dolores. Lorem
+          ipsum dolor sit amet, consectetur adipisicing elit. Dolor, rerum
+          quidem molestias fugit eligendi nihil unde recusandae ipsa laudantium
+          minima aspernatur dolores laborum! Repudiandae pariatur vitae adipisci
+          maiores doloribus dolores. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Dolor, rerum quidem molestias fugit eligendi nihil
+          unde recusandae ipsa laudantium minima aspernatur dolores laborum!
+          Repudiandae pariatur vitae adipisci maiores doloribus dolores. Lorem
+          ipsum dolor sit amet, consectetur adipisicing elit. Dolor, rerum
+          quidem molestias fugit eligendi nihil unde recusandae ipsa laudantium
+          minima aspernatur dolores laborum! Repudiandae pariatur vitae adipisci
+          maiores doloribus dolores. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Dolor, rerum quidem molestias fugit eligendi nihil
+          unde recusandae ipsa laudantium minima aspernatur dolores laborum!
+          Repudiandae pariatur vitae adipisci maiores doloribus dolores. Lorem
+          ipsum dolor sit amet, consectetur adipisicing elit. Dolor, rerum
+          quidem molestias fugit eligendi nihil unde recusandae ipsa laudantium
+          minima aspernatur dolores laborum! Repudiandae pariatur vitae adipisci
+          maiores doloribus dolores. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Dolor, rerum quidem molestias fugit eligendi nihil
+          unde recusandae ipsa laudantium minima aspernatur dolores laborum!
+          Repudiandae pariatur vitae adipisci maiores doloribus dolores. Lorem
+          ipsum dolor sit amet, consectetur adipisicing elit. Dolor, rerum
+          quidem molestias fugit eligendi nihil unde recusandae ipsa laudantium
+          minima aspernatur dolores laborum! Repudiandae pariatur vitae adipisci
+          maiores doloribus dolores. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Dolor, rerum quidem molestias fugit eligendi nihil
+          unde recusandae ipsa laudantium minima aspernatur dolores laborum!
+          Repudiandae pariatur vitae adipisci maiores doloribus dolores. Lorem
+          ipsum dolor sit amet, consectetur adipisicing elit. Dolor, rerum
+          quidem molestias fugit eligendi nihil unde recusandae ipsa laudantium
+          minima aspernatur dolores laborum! Repudiandae pariatur vitae adipisci
+          maiores doloribus dolores.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Description;
