49b879ed1a7a744a6cdb3d207259b65e0e612730
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-30 18:30:30 +0300

    Vehicle History Component created

---
+++ src/components/layout/Pages/ProductPage/ProductSection/VehicleHistory/VehicleHistory.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReviewCards from './ReviewCards/ReviewCards';
+import classes from './VehicleHistory.module.scss';
+
+const VehicleHistory = ({
+  owners,
+  title,
+  accidents,
+  rentalUse,
+  autoCheckDate,
+}) => {
+  return (
+    <div className={classes.VehicleHistory}>
+      <h3>Vehicle History</h3>
+      <ReviewCards
+        owners={owners}
+        title={title}
+        accidents={accidents}
+        rentalUse={rentalUse}
+      />
+      <div className={classes.TextSection}>
+        <a
+          href='#!'
+          title='AutoCheck Vehicle History Report'
+          referrerPolicy='no-referrer'
+          target='_blank'
+        >
+          Save 20% on the full AutoCheck Vehicle History Report
+        </a>
+        <p>
+          Vehicle history data provided by Experian AutoCheck on {autoCheckDate}
+          . This data, and any reliance on it, is subject to the{' '}
+          <a
+            href='#!'
+            title='AutoCheck Terms and Conditions'
+            referrerPolicy='no-referrer'
+            target='_blank'
+          >
+            AutoCheck Terms and Conditions
+          </a>{' '}
+          and the CarWorld Terms of Use.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+VehicleHistory.defaultProps = {
+  owners: 0,
+  title: false,
+  accidents: 0,
+  rentalUse: false,
+};
+
+VehicleHistory.propTypes = {
+  owners: PropTypes.number,
+  title: PropTypes.bool,
+  accidents: PropTypes.number,
+  rentalUse: PropTypes.bool,
+};
+
+export default VehicleHistory;
