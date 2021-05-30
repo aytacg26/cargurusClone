bdc9e34230e7bbaf61e81ba305359e722c6a139f
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-30 18:30:05 +0300

    Zoom functionality removed from component

--- src/components/layout/Pages/ProductPage/ProductSection/DetailsTable/DetailGroup/DetailGroup.js
+++ src/components/layout/Pages/ProductPage/ProductSection/DetailsTable/DetailGroup/DetailGroup.js
@@ -1,24 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import classes from './DetailGroup.module.scss';
-import { FaWindowClose } from 'react-icons/fa';
 
 const DetailGroup = ({ label, data }) => {
-  const [zoomIn, setZoomIn] = useState(false);
-
   let dataContent;
-
-  useEffect(() => {
-    let timer;
-    if (zoomIn) {
-      timer = setTimeout(() => {
-        setZoomIn(false);
-      }, 6000);
-    }
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [zoomIn]);
 
   if (typeof data === 'string') {
     dataContent = <p>{data}</p>;
@@ -30,20 +14,8 @@ const DetailGroup = ({ label, data }) => {
     return null;
   }
 
-  const handleZoom = () => {
-    setZoomIn((prevState) => !prevState);
-  };
-
   return (
-    <div
-      className={`${classes.DetailGroup} ${zoomIn ? classes.Zoom : ''}`}
-      onClick={handleZoom}
-    >
-      {zoomIn && (
-        <span className={classes.Close}>
-          <FaWindowClose color='#fff' />
-        </span>
-      )}
+    <div className={classes.DetailGroup}>
       <label>{label}</label>
       <div className={classes.DetailData}>{dataContent}</div>
     </div>
