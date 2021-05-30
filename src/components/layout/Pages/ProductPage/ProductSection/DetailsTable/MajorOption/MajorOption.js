d944ea755e4c90d32a2276ddb6fb93bb7c1b8a54
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-30 18:31:11 +0300

    Tooltip removed and InfoTooltip Created

--- src/components/layout/Pages/ProductPage/ProductSection/DetailsTable/MajorOption/MajorOption.js
+++ src/components/layout/Pages/ProductPage/ProductSection/DetailsTable/MajorOption/MajorOption.js
@@ -1,42 +1,14 @@
-import React, { useState } from 'react';
-import Tooltip from '../../../../../../ui/Tooltip/Tooltip';
+import InfoTooltip from '../../../../../../ui/InfoTooltip/InfoTooltip';
 import classes from './MajorOption.module.scss';
 
 const MajorOption = ({ label, info, isExist, toolTipSide }) => {
-  const [show, setShow] = useState(false);
   const icon = `${classes.CheckMark} ${isExist ? classes.Exist : ''}`;
-  let timerLeave;
-  let timerEnter;
-
-  const hanldeMouseEnter = () => {
-    clearTimeout(timerLeave);
-    timerEnter = setTimeout(() => setShow(true), 500);
-  };
-
-  const handleMouseLeave = () => {
-    clearTimeout(timerEnter);
-    timerLeave = setTimeout(() => setShow(false), 1000);
-  };
 
   return (
     <div className={classes.MajorOption}>
       <span className={icon}></span>
       <label>{label}</label>
-      {info && (
-        <Tooltip
-          text={info}
-          show={show}
-          onMouseEnter={hanldeMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          side={toolTipSide}
-        >
-          <span
-            className={classes.InfoIcon}
-            onMouseEnter={hanldeMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          ></span>
-        </Tooltip>
-      )}
+      <InfoTooltip info={info} toolTipSide={toolTipSide} />
     </div>
   );
 };
