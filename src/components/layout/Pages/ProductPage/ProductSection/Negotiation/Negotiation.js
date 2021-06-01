e6e15304ce14ac52bb2daab6a0908d7a2083916f
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-06-01 19:21:58 +0300

    priceStats added and init and last price and price drop calculation added

--- src/components/layout/Pages/ProductPage/ProductSection/Negotiation/Negotiation.js
+++ src/components/layout/Pages/ProductPage/ProductSection/Negotiation/Negotiation.js
@@ -2,18 +2,43 @@ import React from 'react';
 import CardWindow from '../../../../../ui/CardWindow/CardWindow';
 import NegotiationCards from './NegotiationCards/NegotiationCards';
 import classes from './Negotiation.module.scss';
+import PriceStatistics from './PriceStatistics/PriceStatistics';
 
-const Negotiation = ({ listDate, numberOfSaves, priceStatistics }) => {
+const Negotiation = ({
+  listDate,
+  numberOfSaves,
+  priceStatistics,
+  currency,
+}) => {
+  let priceDrop = 0;
+
+  if (priceStatistics.length > 1) {
+    const initPrice = priceStatistics[0].price;
+    const lastPrice = priceStatistics[priceStatistics.length - 1].price;
+    priceDrop =
+      parseFloat(initPrice).toFixed(2) - parseFloat(lastPrice).toFixed(2);
+  }
+
   return (
     <CardWindow
       header='Negotiation'
       cards={
-        <NegotiationCards listDate={listDate} numberOfSaves={numberOfSaves} />
+        <NegotiationCards
+          listDate={listDate}
+          numberOfSaves={numberOfSaves}
+          priceDrop={priceDrop}
+          currency={currency}
+        />
       }
     >
       {priceStatistics && (
         <div className={classes.Negotiation}>
-          <div>This part will contain price drop statistics</div>
+          <div>
+            <PriceStatistics
+              priceStatistics={priceStatistics}
+              currency={currency}
+            />
+          </div>
           <span>
             Price History comprises prices posted on CarWorld for this vehicle.
             Price may exclude certain taxes, fees, and/or charges. See seller
