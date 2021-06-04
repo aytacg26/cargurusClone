e6f158589a975f391cb6ff20591bc357b3f79527
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-06-04 17:57:56 +0300

    Filter Search added to Reviews

--- src/components/layout/Pages/ProductPage/ProductSection/DealerReviews/DealerReviews.js
+++ src/components/layout/Pages/ProductPage/ProductSection/DealerReviews/DealerReviews.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Fragment } from 'react';
 import Button from '../../../../../ui/Button/Button';
 import ReviewBars from '../../../../../ui/ReviewBars/ReviewBars';
 import UserReview from '../../../../../ui/UserReview/UserReview';
+import Input from '../../../../../ui/Input/Input';
 import classes from './DealerReviews.module.scss';
 
 const DealerReviews = ({ reviews, header, onDelete }) => {
+  const [showReviews, setShowReviews] = useState(false);
+  const [search, setSearch] = useState('');
+
   if (!reviews || reviews.length === 0) {
     return <h3>No Reviews For This Dealer Yet.</h3>;
   }
+
+  const handleReviewShow = () => {
+    setShowReviews((prevState) => !prevState);
+  };
+
+  const handleSearch = (e) => {
+    const { value } = e.target;
+    setSearch(value);
+  };
+
+  console.log(reviews);
 
   return (
     <Fragment>
@@ -20,16 +35,36 @@ const DealerReviews = ({ reviews, header, onDelete }) => {
           <Button
             btnType='text'
             style={{ paddingRight: 0 }}
-          >{`Read ${reviews.length} Reviews`}</Button>
+            onClick={handleReviewShow}
+          >
+            {!showReviews ? `Read ${reviews.length} Reviews` : 'Hide reviews'}
+          </Button>
         </div>
       </div>
-      {reviews.map((review) => (
-        <UserReview
-          key={review.id}
-          review={review}
-          onDelete={() => onDelete(review.id)}
-        />
-      ))}
+      {showReviews && (
+        <Fragment>
+          <Input
+            type='search'
+            placeholder='Search in reviews...'
+            style={{ border: '1px solid #ccc' }}
+            value={search}
+            onChange={handleSearch}
+          />
+          {reviews
+            .filter(
+              (reviews) =>
+                reviews.text.includes(search) ||
+                reviews.user.fullName.includes(search)
+            )
+            .map((review) => (
+              <UserReview
+                key={review.id}
+                review={review}
+                onDelete={() => onDelete(review.id)}
+              />
+            ))}
+        </Fragment>
+      )}
     </Fragment>
   );
 };
