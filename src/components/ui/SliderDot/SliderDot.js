5eed449617863ef113de380ef677239cfd075355
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-06-07 19:16:09 +0300

    click functionality added to Slider Dots

--- src/components/ui/SliderDot/SliderDot.js
+++ src/components/ui/SliderDot/SliderDot.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import classes from './SliderDot.module.scss';
 
-const SliderDot = ({ isActive }) => {
+const SliderDot = ({ isActive, onClick }) => {
   const dotClass = `${classes.SliderDot} ${isActive ? classes.Active : ''}`;
 
-  return <span className={dotClass}></span>;
+  return <span className={dotClass} onClick={onClick}></span>;
 };
 
 export default SliderDot;
